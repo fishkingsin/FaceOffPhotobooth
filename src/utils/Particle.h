568d348c28695e159bbd5d41ffa1c832c31bb727
James Kong <fishkingsin@gmail.com>
2012-09-13 21:51:16 +0800

    added Particle.h

---
+++ src/utils/Particle.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "ofMain.h"
+#include "ofxTween.h"
+#include "ofxImageSequence.h"
+enum particleMode{
+	PARTICLE_MODE_ATTRACT = 0,
+	PARTICLE_MODE_REPEL,
+	PARTICLE_MODE_NEAREST_POINTS,
+	PARTICLE_MODE_NOISE
+};
+
+class Particle{
+
+	public:
+		Particle();
+		
+		void setMode(particleMode newMode);	
+
+
+		void reset();
+		void update();
+		void draw();		
+		
+		ofPoint pos;
+		ofPoint vel;
+		ofPoint frc;
+		
+		float drag; 
+		float uniqueVal;
+		float scale;
+		
+		particleMode mode;
+    int initTime;
+    ofxTween tween;
+    ofxTween tween2;
+    ofxEasingCubic 	easing;
+    ofxImageSequence *image;
+    int id;
+};
